b9fbd0a5a679b3569cda6faf7bbdedd07a11ddf0
LC <100930523@qq.com>
2018-05-10 10:28:10 +0800

    修改后版本

--- stereo_calib/stereo_calib.cpp
+++ stereo_calib/stereo_calib.cpp
@@ -15,6 +15,61 @@ stereo_calib::stereo_calib(QWidget *parent)
 }
 
 
+QImage stereo_calib::Mat2QImage(const cv::Mat& mat)
+{
+	// 8-bits unsigned, NO. OF CHANNELS = 1
+	if (mat.type() == CV_8UC1)
+	{
+		QImage image(mat.cols, mat.rows, QImage::Format_Indexed8);
+		// Set the color table (used to translate colour indexes to qRgb values)
+		image.setColorCount(256);
+		for (int i = 0; i < 256; i++)
+		{
+			image.setColor(i, qRgb(i, i, i));
+		}
+		// Copy input Mat
+		uchar *pSrc = mat.data;
+		for (int row = 0; row < mat.rows; row++)
+		{
+			uchar *pDest = image.scanLine(row);
+			memcpy(pDest, pSrc, mat.cols);
+			pSrc += mat.step;
+		}
+		return image;
+	}
+	// 8-bits unsigned, NO. OF CHANNELS = 3
+	else if (mat.type() == CV_8UC3)
+	{
+		// Copy input Mat
+		const uchar *pSrc = (const uchar*)mat.data;
+		// Create QImage with same dimensions as input Mat
+		QImage image(pSrc, mat.cols, mat.rows, mat.step, QImage::Format_RGB888);
+		return image.rgbSwapped();
+	}
+	else if (mat.type() == CV_8UC4)
+	{
+		qDebug() << "CV_8UC4";
+		// Copy input Mat
+		const uchar *pSrc = (const uchar*)mat.data;
+		// Create QImage with same dimensions as input Mat
+		QImage image(pSrc, mat.cols, mat.rows, mat.step, QImage::Format_ARGB32);
+		return image.copy();
+	}
+	else
+	{
+		qDebug() << "ERROR: Mat could not be converted to QImage.";
+		return QImage();
+	}
+}
+
+
+void stereo_calib::image_show(const cv::Mat& img)
+{
+	QPixmap pixmap = QPixmap::fromImage(Mat2QImage(img));
+	pixmap = pixmap.scaled(ui.label->size(), Qt::KeepAspectRatio, Qt::SmoothTransformation);
+	ui.label->setPixmap(pixmap);
+}
+
 void stereo_calib::create_object_points(int row, int col, int d)
 {
 	//creat object points
@@ -83,7 +138,7 @@ void stereo_calib::on_actionCalib_triggered()
 				cv::putText(img_color_t, name_.toStdString(), cv::Point(50, 50),
 					cv::FONT_HERSHEY_SIMPLEX, 1, cv::Scalar(0, 0, 255), 2);
 
-				ui.label->setPixmap(QPixmap::fromImage(Mat2QImage(img_color_t)));
+				image_show(img_color_t);
 				QApplication::processEvents();
 #endif
 			}
